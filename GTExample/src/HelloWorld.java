68f149c78fc7005d1ed51533c6639661fd624ac6
c.venkatesh.bhomkar <c.venkatesh.bhomkar@accenture.com>
2017-05-08 18:29:15 +1000

    New Class

--- GTExample/src/HelloWorld.java
+++ GTExample/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello everyone, this is my new project");
+		System.out.println("Hello everyone, this is my new project");
 	}
 }
